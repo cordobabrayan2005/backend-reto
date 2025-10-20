ed573d4fda23123358763176dab54491bcf03de7
bryan_cr <cordoba.brayan@correounivalle.edu.co>
2025-10-19 20:28:09 -0500

    cambio

--- api/index.ts
+++ api/index.ts
@@ -18,31 +18,9 @@ app.get("/videos/popular", async (_req: Request, res: Response) => {
   }
 });
 
-app.get("/peliculas", async (_req: Request, res: Response) => {
-  try {
-    const data = await client.videos.popular({ per_page: 3 });
-    res.json(data);
-  } catch (err) {
-    res.status(500).json({ error: "Failed to fetch popular videos" });
-  }
-});
-
-app.get("/videos/search", async (req: Request, res: Response) => {
-  const query = (req.query.query as string) || "nature";
-  const per_page = Number(req.query.per_page) || 3;
-
-  try {
-    const data = await client.videos.search({ query, per_page });
-    res.json(data);
-  } catch {
-    res.status(500).json({ error: "Failed to search videos" });
-  }
-});
-
 app.get("/", (_req, res) => {
   res.send("✅ Backend corriendo correctamente en Vercel");
 });
 
-// No uses app.listen()
 export const handler = serverless(app);
 export default handler;
